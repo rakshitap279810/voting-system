09d5f9120c9c28447870f48d0cadec237c214132
rakshitap279810 <rakshitap2798@gmail.com>
2026-09-12 11:59:22 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,5 +1,7 @@
 age=int(input("Enter your age"))
 if age < 18:
   print("you are not eligible")
-else:
+  print("you have to wait until become 18 year old")
+else: 
   print("you are eligible")
+  print("you can vote")
